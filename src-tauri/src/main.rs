54179251907fee666b145a516bee936c220bbd75
David Chung <davidc4747@yahoo.com>
2023-06-27 11:39:25 -0700

    Wait for ffmpeg to finish before returning

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,42 +4,40 @@
 use std::process::Command;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![ffgif])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![ffgif])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
-
-
-
-
-
 
 const FFMPEG_PATH: &str = "../public/ffmpeg";
 // const FFPROBE_PATH: &str =  "../public/ffprobe";
 
 #[tauri::command]
 fn ffgif(input_file: &str, start_time: u32, duration: u32) -> String {
-  const FPS: u8 = 10;
-  const RESOLUTION: u16 = 720;
+    const FPS: u8 = 10;
+    const RESOLUTION: u16 = 720;
 
-  let output_file = format!("{}.gif", input_file);
-  let video_filter = format!("fps={},scale={}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse", FPS, RESOLUTION);
-  Command::new(FFMPEG_PATH)
-  .args([
-    "-ss",
-    &start_time.to_string(),
-    "-t",
-    &duration.to_string(),
-    "-y", // Override 
-    "-i",
-    &input_file,
-    "-vf",
-    &video_filter,
-    &output_file
-  ])
-  .spawn()
-  .expect("failed to execute process");
-  
-  output_file.into()
+    let output_file = format!("{}.gif", input_file);
+    let video_filter = format!(
+        "fps={},scale={}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse",
+        FPS, RESOLUTION
+    );
+    Command::new(FFMPEG_PATH)
+        .args([
+            "-ss",
+            &start_time.to_string(),
+            "-t",
+            &duration.to_string(),
+            "-y", // Override
+            "-i",
+            &input_file,
+            "-vf",
+            &video_filter,
+            &output_file,
+        ])
+        .output()
+        .expect("failed to execute process");
+
+    output_file.into()
 }
